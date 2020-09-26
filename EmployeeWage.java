6e827d43b79681ae6fdb2b1fee4ee73d17eb7299
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-26 12:31:22 +0530

    [Gaurav]REFACTORED: Total Employee Wage till given Condition Met

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,32 @@
 public class EmployeeWage
 {
 
-	public static final int IS_PART_TIME = 1;
+		public static final int IS_PART_TIME = 1;
 
-	public static final int IS_FULL_TIME = 2;
+		public static final int IS_FULL_TIME = 2;
 
-	public static final int EMP_RATE_PER_HOUR = 20;		
-
-	public static final int NUM_OF_WORKING_DAYS = 20;
+		public static final int EMP_RATE_PER_HOUR = 20;		
+		
+		public static final int NUM_OF_WORKING_DAYS = 20;
+	
+		public static final int MAX_HRS_IN_MONTH = 100;
 		
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation program");
+
+		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		int empHrs =  0;
 		
-		int empWage = 0;
+		int  totalEmpHrs = 0;
 
-		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
 
-		for(int day = 1; day<=NUM_OF_WORKING_DAYS; day++)
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+
+			totalWorkingDays++;
+
 			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 			
 			switch (empCheck)
@@ -34,12 +40,12 @@ public class EmployeeWage
 				default:
 					empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage +=  empWage;
 
-			System.out.println("Employee Wage for day " + day + ": " + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Days: " + totalWorkingDays + " Emp Hr: " + empHrs);
 
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
