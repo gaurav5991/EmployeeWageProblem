7c088a20104e7d696e36a9ee2a1586de363927ea
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-26 23:57:30 +0530

    [Gaurav]REFACTORED: Added declaration for getTotalWage Method

--- computeEmpWageInterface.java
+++ computeEmpWageInterface.java
@@ -2,4 +2,5 @@ public interface computeEmpWageInterface
 {
 	public void addCompanyEmpWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHrsInMonth);
 	public void computeWage();
+	public int getTotalWage(String company);
 }
