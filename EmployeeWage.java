3e5a036acd3cd1bdd8ee871d5d96b1d251165b6b
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-26 13:50:49 +0530

    [Gaurav]REFACTORED: Multiple companies Employee Wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,12 +4,6 @@ public class EmployeeWage
 		public static final int IS_PART_TIME = 1;
 
 		public static final int IS_FULL_TIME = 2;
-
-		public static final int EMP_RATE_PER_HOUR = 20;		
-		
-		public static final int NUM_OF_WORKING_DAYS = 20;
-	
-		public static final int MAX_HRS_IN_MONTH = 100;
 		
 	public static void main(String[] args)
 	{
