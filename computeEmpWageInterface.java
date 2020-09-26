90e97da8e10b1370d4dfb05428e831689e522342
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-26 20:23:32 +0530

    [Gaurav]REFACTORED: Method Declaration in interface

---
+++ computeEmpWageInterface.java
@@ -0,0 +1,5 @@
+public interface computeEmpWageInterface 
+{
+	public void addCompanyEmpWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHrsInMonth);
+	public void computeWage();
+}
